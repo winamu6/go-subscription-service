b085047986ac3806b44389f3f9eada173319834b
Winamu6 <29032006art@mail.ru>
2025-11-05 00:34:11 +0300

    add db

---
+++ internal/db/db.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"subscription-service/internal/config"
+)
+
+func Connect(cfg *config.Config) *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
+		cfg.DBHost, cfg.DBUser, cfg.DBPass, cfg.DBName, cfg.DBPort,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	log.Println("Database connected")
+	return db
+}
