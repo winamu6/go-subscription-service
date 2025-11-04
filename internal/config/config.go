618c9d2d0ad99e48bec75ba5479b80c9d41801f7
Winamu6 <29032006art@mail.ru>
2025-11-05 00:13:35 +0300

    add config file

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppPort string
+	DBHost  string
+	DBPort  string
+	DBUser  string
+	DBPass  string
+	DBName  string
+}
+
+func Load() *Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println(".env file not found:(")
+	}
+
+	return &Config{
+		AppPort: getEnv("APP_PORT", "8080"),
+		DBHost:  getEnv("DB_HOST", "localhost"),
+		DBPort:  getEnv("DB_PORT", "5432"),
+		DBUser:  getEnv("DB_USER", "postgres"),
+		DBPass:  getEnv("DB_PASS", ""),
+		DBName:  getEnv("DB_NAME", "subscription"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
