f43517e52710a91030a95ff9cff293ed6dfb0988
Winamu6 <29032006art@mail.ru>
2025-11-07 00:19:16 +0300

    add auto generated created time and updated time

--- internal/model/subscription.go
+++ internal/model/subscription.go
@@ -8,13 +8,24 @@ import (
 )
 
 type Subscription struct {
-	ID             uint           `gorm:"primaryKey" json:"id"`
-	ServiceName    string         `gorm:"type:varchar(255);not null" json:"service_name"`
-	Price          float64        `gorm:"not null" json:"price"`
-	UserID         uuid.UUID      `gorm:"type:uuid;not null" json:"user_id"`
-	StartDate      time.Time      `gorm:"not null" json:"start_date"`
-	EndDate        *time.Time     `json:"end_date,omitempty"`
-	CreatedAt      time.Time      `json:"created_at"`
-	UpdatedAt      time.Time      `json:"updated_at"`
-	DeletedAt      gorm.DeletedAt `gorm:"index" json:"-"`
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	ServiceName string         `gorm:"type:varchar(255);not null" json:"service_name"`
+	Price       float64        `gorm:"not null" json:"price"`
+	UserID      uuid.UUID      `gorm:"type:uuid;not null" json:"user_id"`
+	StartDate   time.Time      `gorm:"not null" json:"start_date"`
+	EndDate     *time.Time     `json:"end_date,omitempty"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+func (s *Subscription) BeforeCreate(tx *gorm.DB) (err error) {
+	s.CreatedAt = time.Now()
+	s.UpdatedAt = time.Now()
+	return
+}
+
+func (s *Subscription) BeforeUpdate(tx *gorm.DB) (err error) {
+	s.UpdatedAt = time.Now()
+	return
 }
