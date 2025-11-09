a251830e49cb17c9811e3508ef727a5a4ad05abc
Winamu6 <29032006art@mail.ru>
2025-11-09 04:50:06 +0300

    add router

---
+++ internal/router/router.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"subscription-service/internal/handler"
+	"subscription-service/internal/service"
+)
+
+func NewRouter(readSvc service.SubscriptionQueryService, writeSvc service.SubscriptionCommandService) *gin.Engine {
+	r := gin.Default()
+
+	readHandler := handler.NewSubscriptionReadHandler(readSvc)
+	writeHandler := handler.NewSubscriptionWriteHandler(writeSvc)
+
+	subscriptions := r.Group("/subscriptions")
+	{
+		subscriptions.GET("", readHandler.GetAll)
+		subscriptions.GET("/:id", readHandler.GetByID)
+		subscriptions.GET("/user/:user_id", readHandler.GetByUserID)
+		subscriptions.GET("/sum", readHandler.SumPriceByFilter)
+
+		subscriptions.POST("", writeHandler.Create)
+		subscriptions.PUT("/:id", writeHandler.Update)
+		subscriptions.DELETE("/:id", writeHandler.Delete)
+	}
+
+	return r
+}
